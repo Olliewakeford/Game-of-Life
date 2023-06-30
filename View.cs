d3a8e2277dc9e28a188a828f798e60b3e51ea890
Olliewakeford <Oliverwakeford2001@gmail.com>
2023-06-30 12:34:45 +0200

    Create Game of Life Window

---
+++ View.cs
@@ -0,0 +1,21 @@
+using Gdk;
+using Gtk;
+
+class MyWindow : Gtk.Window {
+    public MyWindow() : base("Game of Life") {
+    }
+
+    protected override bool OnDeleteEvent(Event e) {
+        Application.Quit();
+        return true;
+    }
+}
+
+class Hello {
+    static void Main() {
+        Application.Init();
+        MyWindow w = new MyWindow();
+        w.ShowAll();
+        Application.Run();
+    }
+}
