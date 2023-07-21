0ce2e4d6229219fb694f35fa4e8f4fec3a7fbd7c
Olliewakeford <Oliverwakeford2001@gmail.com>
2023-07-21 10:51:16 +0100

    Add Grid Class
    A class that can hold a grid as a rectangular array of a specified height and width. Can either populate the grid with random 0's and 1s, or create a clear grid.

--- Model.cs
+++ Model.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+static class Grid {
+    //return a grid of 0s
+    public static int[,] clearGrid(int height, int width){
+        int[,] grid = new int[height, width];
+
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++)
+                grid[i,j] = 0;
+        }
+        
+        return grid;
+    }
+    
+    //return a grid of random 0s and 1s
+    public static int[,] randomGrid(int height, int width){
+        Random rand = new Random();
+        int[,] grid = new int[height, width];
+
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                grid[i, j] = rand.Next(0,2); //look into adjusting probability
+            }
+        }
+        
+        return grid;
+    }
+}
